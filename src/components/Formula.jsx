d5de94d559b4dd1626bf0d9c5f63989cccd2247e
ndjerrou <n.djerrou@gmail.com>
2022-06-23 14:34:58 +0100

    getting products

--- src/components/Formula.jsx
+++ src/components/Formula.jsx
@@ -1,10 +1,38 @@
 import axios from 'axios'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
 
-export default function () {
+
+
+
+export default  function () {
   const [name, setName] = useState('')
   const [price, setPrice] = useState(0)
+
+  const [products, setProducts] = useState(null)
+
+
+  useEffect( ()=>{
+    // with the [], get fired once the component is mounted into the DOM
+    console.log('after mounted into the dom')
+
+  // // getting the products...
+
+  async function getProducts(){
+    try{
+      const result = await axios.get("http://localhost:3001/reseller/list-products") //side-effects
+      setProducts(result.data) // provquer un re-render = la fonction du composant s'éxécute à nouveau
+    }
+    catch(err){
+      console.error(err.message)
+    }
+  }
+
+  getProducts()
+  }, [])
+
+  console.log('render formula C')
+
 
   function handleChangeName(e){
     setName(e.target.value)
@@ -20,21 +48,38 @@ export default function () {
     name,
     price
     }) 
-
-    console.log(resultat)
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Nom du produit<input value={name} onChange={handleChangeName}/>
+          </label>
+          </div>
+        <div>
+          <label>Prix du produit<input value={price} onChange={handleChangePrice} type="number" />
+          </label>
+          </div>
+        <button>Créer mon produit</button>
+      </form>
       <div>
-        <label>Nom du produit<input value={name} onChange={handleChangeName}/>
-        </label>
-        </div>
-      <div>
-        <label>Prix du produit<input value={price} onChange={handleChangePrice} type="number" />
-        </label>
-        </div>
-      <button>Créer mon produit</button>
-    </form> 
+        <h2>Mes produits à vendre</h2>
+        {products && <ul>
+          { products.map((product)=><li>{product.name} - {product.price} €</li>)}
+      </ul>
+      }
+      </div>
+    </div>
   );
 }
+
+// EX - supprimer un produit affiché
+
+// Ajouter un bouton/icone à côté de mon produit
+
+// Récupérer le clic sur ce bouton/icone
+
+// Déclencher une requête vers le back pour supprimer ce produit
+
+// Question : le produit est supprimé du back, comment refléter ça côté front ?
